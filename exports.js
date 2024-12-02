502941cd77444035a4e08f143d74ff741fb25fe0
Aqua200 <chinquepapa@gmail.com>
2024-12-02 07:00:11 -0800

    Create exports.js

---
+++ exports.js
@@ -0,0 +1,25 @@
+const iconUrls = [
+    "https://raw.githubusercontent.com/Aqua200/Prueba/refs/heads/main/src/img/icon1.jpg",
+    "https://raw.githubusercontent.com/Aqua200/Prueba/refs/heads/main/src/img/icon2.jpg",
+    "https://raw.githubusercontent.com/Aqua200/Prueba/refs/heads/main/src/img/icon3.jpg",
+    "https://raw.githubusercontent.com/Aqua200/Prueba/refs/heads/main/media/img/icon4.jpg",
+    "https://raw.githubusercontent.com/Aqua200/Prueba/refs/heads/main/media/img/icon5.jpg"
+];
+console.log('exports load √');
+export const getRandomIcon = () => {
+    const randomIcon = Math.floor(Math.random() * iconUrls.length);
+    return iconUrls[randomIcon];
+};
+
+const thumbUrls = [
+    "https://raw.githubusercontent.com/Aqua200/Prueba/refs/heads/main/src/img/thumbnail1.jpg",
+    "https://raw.githubusercontent.com/Aqua200/Prueba/refs/heads/main/src/img/thumbnail2.jpg",
+    "https://raw.githubusercontent.com/Aqua200/Prueba/refs/heads/main/src/img/thumbnail3.jpg",
+    "https://raw.githubusercontent.com/Aqua200/Prueba/refs/heads/main/src/img/thumbnail4.jpg",
+    "https://raw.githubusercontent.com/Aqua200/Prueba/refs/heads/main/src/img/thumbnail5.jpg"
+];
+
+export const getRandomThumb = () => {
+    const randomThumb = Math.floor(Math.random() * thumbUrls.length);
+    return thumbUrls[randomThumb];
+};
